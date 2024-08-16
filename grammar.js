970fa0a03788e648215a3b4155924db1933af9af
maxmysten <max.spector@mystenlabs.com>
2024-08-16 06:44:38 -0400

    Receiver_call will try to match type_arguments wrong
    If you have a dot expression followed by a "<", instead of this being
    a binary expression, it tires to turn this into an apply_type
    or type_arguments and errors out.

--- grammar.js
+++ grammar.js
@@ -848,7 +848,7 @@ module.exports = grammar({
     )),
     receiver_call: $ => prec.left(PRECEDENCE.call, seq(
       field('receiver', $._dot_or_index_chain), '.', field('func', $.identifier),
-      optional(field('type_arguments', $.type_arguments)),
+      optional(field('type_generics', seq('::', $.type_arguments))),
       field('arguments', $.arg_list),
     )),
     receiver_macro_call: $ => prec.left(PRECEDENCE.call, seq(
